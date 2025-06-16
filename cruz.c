6894b9f264dd17e587c5aabdb73b3741115a6a1e
cvazcosta <cvazcosta2001@gmail.com>
2025-06-16 14:11:24 -0300

    Desenvolvendo programa que desenha uma matriz em cruz

--- cruz.c
+++ cruz.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
 
+#define LINHAS 3
+#define COLUNAS 5
+
 int main()
 {
-  // A ser desenvolvido
+  int l, c;
+  int colunaCentral = COLUNAS / 2;
+  int linhaCentral = LINHAS / 2;
+  int soma = 0;
+
+  for (l = 0; l < LINHAS; l++)
+  {    
+    if (l == linhaCentral)
+    {
+      soma = colunaCentral;
+    } else
+    {
+      soma = 0;
+    }
+
+    for (c = 0; c < COLUNAS; c++)
+    {      
+      if ((c >= (colunaCentral - soma)) && (c <= (colunaCentral + soma)))
+      {
+        printf("1 ");
+      }
+      else
+      {
+        printf("0 ");
+      }      
+    }
+    printf("\n");
+  }
+
+  return (0);
 }
